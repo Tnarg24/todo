9c6343becf8f84596310187b2797ba0f8864e883
Tnarg24 <104528967+Tnarg24@users.noreply.github.com>
2022-09-06 20:07:37 +0100

    .

--- index.php
+++ index.php
@@ -1,10 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+        <title>MyFirstToDoApp</title>
 
-echo "Hello World!";
+</head>
 
-echo "RT Was Ere 2k22 👍";
+<body>
 
+    <div class="container">
+           
+        <h1>To Do</h1>
 
-?>
+        <div style="padding: 5px 0px 5px 0px">
+            Example task title
+            <div style="padding-top: 2px">Description</div>
+        </div>
 
-<h1>Hello</h1>
+        <div style="padding: 5px 0px 5px 0px">
+            Example task title
+            <div style="padding-top: 2px">Description</div>
+        </div>
+
+        <div style="padding: 5px 0px 5px 0px">
+            Example task title
+            <div style="padding-top: 2px">Description</div>
+        </div>
+
+        <a  href="create_task.html" class="btn btn-primary btn-md">Description</a>
+
+        
+
+    </div>
