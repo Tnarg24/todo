9023aaf2a911a7d06402675b0d1521e18f233dc2
Tnarg24 <104528967+Tnarg24@users.noreply.github.com>
2022-09-07 20:53:32 +0100

    .

--- db_create_task.php
+++ db_create_task.php
@@ -1,23 +1,23 @@
 <?php
 
-// function OpenCon(){
-//     $dbhost = "localhost";
-//     $dbuser = "root";
-//     $dbpass = "1234";
-//     $db = "example";
-//     $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
-//     return $conn;
-// }
- 
-// function CloseCon($conn){
-//     $conn -> close();
-// }
+  
+ mysqli_real_connect($conn, "todoappdb.mysql.database.azure.com", "standishg", "{your_password}", "{your_database}", 3306, MYSQLI_CLIENT_SSL);
+
+ $host = 'todoappdb.mysql.database.azure.com';
+  $username = 'standishg'; $password = 'Test1234?';
+   $db_name = 'appdatabase';
+    //Initializes MySQLi 
+    $conn = mysqli_init(); mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
+    // Establish the connection 
+    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL); 
+    //If connection failed, show the error 
+    if (mysqli_connect_errno()) {     die('Failed to connect to MySQL: '.mysqli_connect_error()); }
 
 
-$title = $_POST['title'];
-$description = $_POST['description'];
+// $title = $_POST['title'];
+// $description = $_POST['description'];
 
-$task = $title ." - ". $description;
+// $task = $title ." - ". $description;
 
-echo json_encode($task);
+// echo json_encode($task);
