501b28f62e773ee7cd5afb68912cb5e209a2f5b2
Tnarg24 <104528967+Tnarg24@users.noreply.github.com>
2022-09-09 20:32:36 +0100

    .

--- db_create_task.php
+++ db_create_task.php
@@ -36,19 +36,19 @@ if (mysqli_connect_errno()) {
 // //{ printf("Table created\n"); }
 
 
-// //Create an Insert prepared statement and run it 
-// $product_name = 'BrandNewProduct';
-// $product_color = 'Blue';
-// $product_price = 15.5;
-// if ($stmt = mysqli_prepare($conn, "
-//     INSERT INTO Products (ProductName, Color, Price)
-//     VALUES (?, ?, ?)
-// ")) {     
-//     mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
-//     mysqli_stmt_execute($stmt);
-//     printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
-//     mysqli_stmt_close($stmt);
-// }
+//Create an Insert prepared statement and run it 
+$product_name = 'Grants Product';
+$product_color = 'Pink';
+$product_price = 15.5;
+if ($stmt = mysqli_prepare($conn, "
+    INSERT INTO Products (ProductName, Color, Price)
+    VALUES (?, ?, ?)
+")) {     
+    mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
+    mysqli_stmt_execute($stmt);
+    printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+    mysqli_stmt_close($stmt);
+}
 
 
 //Run the Select query 
--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+    $host = 'todoappdb.mysql.database.azure.com';
+    $username = 'standishg';
+    $password = 'Test1234?';
+    $db_name = 'appdatabase';
+    //Initializes MySQLi 
+    $conn = mysqli_init(); 
+    mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
+    // Establish the connection 
+    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL); 
+    //If connection failed, show the error 
+    if (mysqli_connect_errno()) {    
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    };
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,6 +34,23 @@
     <div class="container">
            
         <h1>To Do</h1>
+
+    <?php
+
+        //Run the Select query 
+        printf("Reading data from table: \n");
+        $res = mysqli_query($conn, 'SELECT * FROM Products');
+        while ($row = mysqli_fetch_assoc($res)) {
+        //    var_dump($row); 
+
+            echo "Product Name: " . $row["ProductName"];
+            echo "<br /><br />";
+            echo "Color: " . $row["Color"];
+}
+
+    ?>
+
+
 
         <div style="padding: 5px 0px 5px 0px">
             Example task title
