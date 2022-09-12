d43d8d94cd1910ed9a0e1adc4841d344c1268957
Tnarg24 <104528967+Tnarg24@users.noreply.github.com>
2022-09-12 21:15:13 +0100

    .

--- db_create_task.php
+++ db_create_task.php
@@ -8,25 +8,24 @@ include("db_connection.php");
 if (isset($_POST['title']) && isset($_POST['description'])){
 
     // Create an Insert prepared statement and run it 
-    $product_name = 'Grants Product';
-    $product_color = 'Pink';
-    $product_price = 15.5;
-    if ($stmt = mysqli_prepare($conn, "
-        INSERT INTO Products (ProductName, Color, Price)
-        VALUES (?, ?, ?)
+    
+    
+    $res =  if ($stmt = mysqli_prepare($conn, "
+        INSERT INTO tasks (title, description)
+        VALUES (?, ?)
     ")) {     
-        mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
+        mysqli_stmt_bind_param($stmt, 'ssd', $_POST['title'], $_POST['description']);
         mysqli_stmt_execute($stmt);
-        printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+        // printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
         mysqli_stmt_close($stmt);
     }
 
-    $title = $_POST['title'];
-    $description = $_POST['description'];
+    // $title = $_POST['title'];
+    // $description = $_POST['description'];
 
-    $task = $title ." - ". $description;
+    // $task = $title ." - ". $description;
 
-    echo json_encode($task);
+    echo json_encode($res);
 
 }
 
