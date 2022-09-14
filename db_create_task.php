5ff3fc45f65350cbcc1560f180164ccea5702f75
Grant <grantstandish@gmail.com>
2022-09-14 20:43:23 +0100

    .

--- db_create_task.php
+++ db_create_task.php
@@ -15,38 +15,32 @@ if (mysqli_connect_errno()) {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 };
 
-print_r($conn);
-
-$title = $_POST["title"];
-
-$description = $_POST["description"];
-
-echo $title . " " . $description;
+// print_r($conn);
 
 
-    // if (isset($_POST["title"]) && isset($_POST["description"])){
+    if (isset($_POST["title"]) && isset($_POST["description"])){
 
-    //     // Create an Insert prepared statement and run it 
+        // Create an Insert prepared statement and run it 
         
         
-    //     $res =  if ($stmt = mysqli_prepare($conn, "
-    //         INSERT INTO tasks (title, description)
-    //         VALUES (?, ?)
-    //     ")) {     
-    //         mysqli_stmt_bind_param($stmt, 'ssd', $_POST['title'], $_POST['description']);
-    //         mysqli_stmt_execute($stmt);
-    //         // printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
-    //         mysqli_stmt_close($stmt);
-    //     }
+        $res =  if ($stmt = mysqli_prepare($conn, "
+            INSERT INTO tasks (title, description)
+            VALUES (?, ?)
+        ")) {     
+            mysqli_stmt_bind_param($stmt, 'ssd', $_POST['title'], $_POST['description']);
+            mysqli_stmt_execute($stmt);
+            // printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+            mysqli_stmt_close($stmt);
+        }
 
-    //     // $title = $_POST['title'];
-    //     // $description = $_POST['description'];
+        // $title = $_POST['title'];
+        // $description = $_POST['description'];
 
-    //     // $task = $title ." - ". $description;
+        // $task = $title ." - ". $description;
 
-    //     echo json_encode($res);
+        echo json_encode($res);
 
-    // }
+    }
 }
 catch(Exception $e){
     echo " <br /><br /> error: " . $e->getMessage();
