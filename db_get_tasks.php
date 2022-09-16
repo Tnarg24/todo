d24f82b0783c8b04c4df2278a395d6d484db088e
Grant <grantstandish@gmail.com>
2022-09-16 19:56:39 +0100

    .

--- db_get_tasks.php
+++ db_get_tasks.php
@@ -2,27 +2,27 @@
 
 include "db_connection.php";
 
-// //Run the Select query 
-// //printf("Reading data from table: \n");
-// $res = mysqli_query($conn, 'SELECT * FROM Products');
-// while ($row = mysqli_fetch_assoc($res)) {
-// //    var_dump($row); 
-
-//     echo "Product Name: " . $row["ProductName"];
-//     echo "<br /><br />";
-//     echo "Color: " . $row["Color"];
-// }
-
 //Run the Select query 
 //printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM login_page');
+$res = mysqli_query($conn, 'SELECT * FROM Products');
 while ($row = mysqli_fetch_assoc($res)) {
 //    var_dump($row); 
 
-    echo "Product Name: " . $row["email"];
+    echo "Product Name: " . $row["ProductName"];
     echo "<br /><br />";
-    echo "Color: " . $row["password"];
+    echo "Color: " . $row["Color"];
 }
+
+// //Run the Select query 
+// //printf("Reading data from table: \n");
+// $res = mysqli_query($conn, 'SELECT * FROM login_page');
+// while ($row = mysqli_fetch_assoc($res)) {
+// //    var_dump($row); 
+
+//     echo "Product Name: " . $row["email"];
+//     echo "<br /><br />";
+//     echo "Color: " . $row["password"];
+// }
 
 
 
