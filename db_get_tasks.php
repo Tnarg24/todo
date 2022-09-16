9f796493c12aa8168662ee3a7a3f9706df0eacce
Grant <grantstandish@gmail.com>
2022-09-16 19:54:00 +0100

    .

--- db_get_tasks.php
+++ db_get_tasks.php
@@ -2,18 +2,27 @@
 
 include "db_connection.php";
 
+// //Run the Select query 
+// //printf("Reading data from table: \n");
+// $res = mysqli_query($conn, 'SELECT * FROM Products');
+// while ($row = mysqli_fetch_assoc($res)) {
+// //    var_dump($row); 
+
+//     echo "Product Name: " . $row["ProductName"];
+//     echo "<br /><br />";
+//     echo "Color: " . $row["Color"];
+// }
+
 //Run the Select query 
 //printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM Products');
+$res = mysqli_query($conn, 'SELECT * FROM login_page');
 while ($row = mysqli_fetch_assoc($res)) {
 //    var_dump($row); 
 
-    echo "Product Name: " . $row["ProductName"];
+    echo "Product Name: " . $row["email"];
     echo "<br /><br />";
-    echo "Color: " . $row["Color"];
+    echo "Color: " . $row["password"];
 }
-
-
 
 
 
