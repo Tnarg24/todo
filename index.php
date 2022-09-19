d0202667fd491fdc32cc8f860444559667f0ea30
Grant <grantstandish@gmail.com>
2022-09-19 20:40:34 +0100

    .

--- index.php
+++ index.php
@@ -1,31 +1,21 @@
-<?php
-
-include "db_connection.php";
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="styling.css" type="text/css"> 
-    
-    <title>MyFirstToDoApp</title>
-
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
-
 <body>
 
 
-    <?php
+
+<div class="container">
+  <h2 style="margin-bottom: 80px;">ToDo</h2>
+
+        <?php
 
         //Run the Select query 
         //printf("Reading data from table: \n");
@@ -44,12 +34,8 @@ include "db_connection.php";
             ";
         }
 
-    ?>
+        ?>
 
-
-
- <div class="container">
-  <h2 style="margin-bottom: 80px;">ToDo</h2>
   <!-- Trigger the modal with a button -->
   <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Create New</button>
   <a  href="login.html" class="btn btn-info btn-lg">Login</a>
@@ -97,36 +83,30 @@ include "db_connection.php";
   
 </div>
 
-
-
-    <script>
-
-        // save to db
-        $(document).on('click', '#save_button', function(e){
-            e.preventDefault();      
-            
-            $.ajax({
-                url: `db_create_task.php`, 
-                type: 'POST', //Request object
-                data: {
-                    title :  $('#task_title').val(),
-                    description :  $('#task_description').val() 
-                },
-                success: function(result){
-                    console.log(result)
-                }
-            });
-        })
-
-            
-    </script>
-
 </body>
 </html>
 
 
+   
+
+          
 
 
+
+
+
+        
+        
+        
+
+
+
+    
+        
+
+        
+
+    </div>
 
 
 
