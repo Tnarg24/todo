835de19aed3b6b1491d4eef1c04e0b9802136a6e
Grant <grantstandish@gmail.com>
2022-09-19 20:31:03 +0100

    .

--- index.php
+++ index.php
@@ -11,8 +11,6 @@ include "db_connection.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
@@ -23,28 +21,6 @@ include "db_connection.php";
 </head>
 
 <body>
-
-
-    <?php
-
-        //Run the Select query 
-        //printf("Reading data from table: \n");
-        $res = mysqli_query($conn, 'SELECT * FROM tasks');
-        while ($row = mysqli_fetch_assoc($res)) {
-        //    var_dump($row); 
-
-            // echo "Product Name: " . $row["ProductName"];
-            // echo "<br /><br />";
-            // echo "Color: " . $row["Color"];
-
-            echo "<div style='padding: 5px 0px 5px 0px'>
-                ". $row["title"] . "
-                <div style='padding-top: 2px'>". $row["description"] . "</div>
-                </div>
-            ";
-        }
-
-    ?>
 
 
 
@@ -94,6 +70,27 @@ include "db_connection.php";
         </div>
             
     </div> 
+
+    <?php
+
+        //Run the Select query 
+        //printf("Reading data from table: \n");
+        $res = mysqli_query($conn, 'SELECT * FROM tasks');
+        while ($row = mysqli_fetch_assoc($res)) {
+        //    var_dump($row); 
+
+            // echo "Product Name: " . $row["ProductName"];
+            // echo "<br /><br />";
+            // echo "Color: " . $row["Color"];
+
+            echo "<div style='padding: 5px 0px 5px 0px'>
+                ". $row["title"] . "
+                <div style='padding-top: 2px'>". $row["description"] . "</div>
+                </div>
+            ";
+        }
+
+    ?>
 
     <script>
 
