a7c6db0c2cbc16a1c82f3685a876bcb6b6864812
Grant <grantstandish@gmail.com>
2022-09-21 20:39:08 +0100

    .

--- index.php
+++ index.php
@@ -44,112 +44,34 @@ include "db_connection.php";
     ?>
 
 
-
-
-            <div class="modal" tabindex="-1" id="myModal" role="dialog">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Modal body text goes here.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-                </div>
-            </div>
-            </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     <div class="container">
         <h2 style="margin-bottom: 80px;">ToDo</h2>
             <!-- Trigger the modal with a button -->
-        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Create New</button>
-        <a  href="login.html" class="btn btn-info btn-lg">Login</a>
+        <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
 
-            <!-- Modal -->
-        <div class="modal fade" id="myModal1" role="dialog">
-            <div class="modal-dialog">
-                
-                <!-- Modal content-->                
-                <div class="modal-content">
-                    <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Create Task</h4>
-                    </div>
-                    <div class="modal-body">                    
-                                
-                        <form id="survey-form">
-                    
-                            <div class="form-group"> 
-                                <label for="task_title">Title</label>
-                                <input class="form-controlgs" type="text" id="task_title" required>
-                            </div>    
-                            
-                            
-                            <div class="form-group">
-                                <label for="task_description">Task description</label>
-                                <textarea class="input-textarea" id="task_description" name="comment" rows="3" ></textarea>
-                            </div>
-                            
-                        </form> 
-
-                    </div>
-                        
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-default" id="save_button">Save</button>
-                    </div>
-                
-                </div>
-                
-            </div>
-        </div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+            
+        
             
     </div> 
 
