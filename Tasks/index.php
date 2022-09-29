5ef5001796a906e99a8b6f36a70c693428368239
Grant <grantstandish@gmail.com>
2022-09-29 21:07:47 +0100

    .

--- Tasks/index.php
+++ Tasks/index.php
@@ -1,6 +1,18 @@
 <?php
 
-include "../db_connection.php";
+$host = 'todoappdb.mysql.database.azure.com';
+$username = 'standishg';
+$password = 'Test1234?';
+$db_name = 'appdatabase';
+//Initializes MySQLi 
+$conn = mysqli_init(); 
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
+// Establish the connection 
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL); 
+//If connection failed, show the error 
+if (mysqli_connect_errno()) {    
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+};
 
 
 
